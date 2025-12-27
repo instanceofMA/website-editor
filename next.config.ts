67c11ea11238527372e4ece9c29737af66b6368e
instanceofma <ma@grandeur.dev>
2025-12-27 16:24:25 +0500

    Remove default basePath fallback

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/tools/website-editor",
+    /* config options here */
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
 export default nextConfig;
