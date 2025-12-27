0e809bc629d8e8d3aa89279f511a73343fc948dd
instanceofma <ma@grandeur.dev>
2025-12-27 16:28:21 +0500

    Cleanup config comments

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    /* config options here */
+    // Configures the base path for deployment (e.g. /website-editor)
+    // Set NEXT_PUBLIC_BASE_PATH environment variable on Vercel to enable this.
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
